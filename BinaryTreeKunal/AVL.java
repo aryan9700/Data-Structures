89b30a297c60c1a19c60e5edcb30776fb0cd43aa
ARYAN YADAV <96650638+aryan9700@users.noreply.github.com>
2023-10-28 16:09:48 +0530

    hi

--- BinaryTreeKunal/AVL.java
+++ BinaryTreeKunal/AVL.java
@@ -1,4 +1,4 @@
-public class BST {
+public class AVL {
     public class Node{
         private int value;
         private Node left;
@@ -13,7 +13,7 @@ public class BST {
         }
     }
     private Node root;
-    public BST(){}
+    public AVL(){}
     public int height() {
         return height(root);
       }
@@ -53,7 +53,52 @@ public class BST {
             node.right=insert(value, node.right);
         }
         node.height=Math.max(height(node.left), height(node.right)+1);
-        return node;       
+        return rotate(node);       
+    }
+    private Node rotate(Node node) {
+        if(height(node.left)-height(node.right)>1){
+            //Left Heavy
+            if(height(node.left.left)-height(node.left.right) > 0){
+                //left left case
+                return rightRotate(node);
+            }
+             if(height(node.left.left)-height(node.left.right) < 0){
+                //left left case
+                node.left=leftRotate(node.left);
+                return rightRotate(node);
+            }
+        }
+        if(height(node.left)-height(node.right) < -1){
+            //Right Heavy
+            if(height(node.right.left)-height(node.right.right) < 0){
+                //right right case
+                return leftRotate(node);
+            }
+             if(height(node.right.left)-height(node.right.right) > 0){
+                //left right case
+                node.right=rightRotate(node.right);
+                return leftRotate(node);
+            }
+        }
+        return node;
+    }
+    private Node rightRotate(Node p) {
+       Node c=p.left;
+       Node t=c.right;
+       c.right=p;
+       p.left=t;
+       p.height=Math.max(height(p.left),height(p.right) )+1;
+       c.height=Math.max(height(c.left),height(c.right) )+1;
+        return c;
+    }
+    private Node leftRotate(Node c) {
+       Node p=c.right;
+       Node t=p.left;
+       p.left=c;
+       c.right=t;
+       c.height=Math.max(height(c.left),height(c.right) )+1;
+       p.height=Math.max(height(p.left),height(p.right) )+1;
+        return p;
     }
     public boolean balanced(){
         return balanced(root);
@@ -83,9 +128,10 @@ public class BST {
         populateSorted(nums,mid+1,end);
     }
     public static void main(String[] args) {
-        BST tree=new BST();
-        int nums[]={1,2,3,4,5,6,7,8};
-        tree.populateSorted(nums);
-        tree.display();
+       AVL tree=new AVL();
+        for (int i = 0; i < 1000; i++) {
+            tree.insert(i);
+        }
+        System.out.println(tree.height());
     }
 }
